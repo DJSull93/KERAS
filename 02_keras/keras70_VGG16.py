d14eb9b93b05e936b8cef3ee7b0e0d128cd87a59
DJSull93 <digi809@naver.com>
2021-08-20 12:14:22 +0900

    edit

--- 02_keras/keras70_VGG16.py
+++ 02_keras/keras70_VGG16.py
@@ -11,13 +11,30 @@ import pandas as pd
 import numpy as np
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.applications import VGG19
+from tensorflow.keras.applications import VGG16, VGG19
 
-# model = VGG16(weights='imagenet', include_top=False, 
-                # input_shape=(32,32,3))
+model = VGG16(weights='imagenet', include_top=False, 
+                input_shape=(100,100,3))
+model.trainable = False
 
-model = VGG19()
+# model = VGG16()
 
-model.summary()
+# print(len(model.weights)) # 26
+# print(len(model.trainable_weights)) # 26 : T, 0 : F
 
+# model.summary()
 
+# input_1 (InputLayer)         [(None, 224, 224, 3)]     0
+# block1_conv1 (Conv2D)        (None, 224, 224, 64)      1792
+# ...
+# flatten (Flatten)            (None, 25088)             0
+# fc1 (Dense)                  (None, 4096)              102764544
+# fc2 (Dense)                  (None, 4096)              16781312
+# predictions (Dense)          (None, 1000)              4097000
+
+# Total params: 138,357,544
+# Trainable params: 138,357,544
+# Non-trainable params: 0
+
+# fc = fully connected / 이전 레이어의 출력을 "평탄화"하여 
+# 다음 스테이지의 입력이 될 수 있는 단일 벡터로 변환
