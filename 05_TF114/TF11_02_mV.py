f0c01d7a9817987714870e01f9a38115e022e136
DJSull93 <digi809@naver.com>
2021-08-25 13:25:09 +0900

    edit

---
+++ 05_TF114/TF11_02_mV.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import numpy as np
+tf.set_random_seed(66)
+
+x_data = [[73,51,65],
+            [92,98,11],
+            [89,31,33],
+            [99,33,100],
+            [17,66,79]] # (5,3)
+y_data = [[152],[185],[180],[205],[142]]    # (5,1)
+
+
+x = tf.placeholder(tf.float32, shape=[None,3])
+y = tf.placeholder(tf.float32, shape=[None,1])
+
+W = tf.Variable(tf.random.normal([3,1]), name='weight')
+b = tf.Variable(tf.random.normal([1]), name='bias')
+
+# hyporthesis = x * W + b # 행렬 연산 에러 발생 
+hyporthesis = tf.matmul(x, W) + b
+
+cost = tf.reduce_mean(tf.square(y-hyporthesis))
+
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.00004)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for epochs in range(2001):
+    cost_val, hy_val, _ = sess.run([cost, hyporthesis, train],
+        feed_dict={x:x_data, y:y_data})
+    if epochs % 10 == 0:
+        print(epochs, "cost :", cost_val, "\n", hy_val)
+
+sess.close()
